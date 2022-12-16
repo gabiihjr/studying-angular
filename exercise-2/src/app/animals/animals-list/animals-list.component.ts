6544b96c63e47e77ff80365c420713c061980df9
Gabriela Hermenegildo Júnior <gabriela.hermenegil@meta.com.br>
2022-12-16 13:16:56 -0300

    starting exercicio-3

--- exercise-2/src/app/animals/animals-list/animals-list.component.ts
+++ exercise-2/src/app/animals/animals-list/animals-list.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
+import { AnimalsService } from './../animals.service';
+import { UserService } from './../../authentication/user/user.service';
+import { Component, OnInit } from '@angular/core';
+import { Animals } from '../animals';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-animals-list',
   templateUrl: './animals-list.component.html',
   styleUrls: ['./animals-list.component.css']
 })
-export class AnimalsListComponent {
+export class AnimalsListComponent implements OnInit{
+
+  animals$ !: Observable<Animals>;
+
+  constructor(
+    private userService: UserService, private animalsService: AnimalsService
+  ) {}
+
+  ngOnInit(): void {
+    // this.userService.returnUser().subscribe((user) => {
+    //   const userName = user.name ?? '';
+    //   this.animalsService.listaDoUsuario(userName).subscribe((animals) =>{
+    //     this.animals = animals;
+    //   })
+    // })
+
+    this.animals$ = this.userService.returnUser().pipe(
+      switchMap((user) => {
+        const userName = user.name ?? '';
+        return this.animalsService.listaDoUsuario(userName);
+      })
+    )
+  }
 
 }
---
+++ exercise-2/src/app/animals/grid-photos-animals/grid-photos-animals.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Animals } from '../animals';
+
+@Component({
+  selector: 'app-grid-photos-animals',
+  templateUrl: './grid-photos-animals.component.html',
+  styleUrls: ['./grid-photos-animals.component.css']
+})
+export class GridPhotosAnimalsComponent implements OnInit{
+
+  @Input() animals!: Animals;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
