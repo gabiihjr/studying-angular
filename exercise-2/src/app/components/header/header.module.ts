cf391d972cb78a926da2460eeeaea4f676daa9e0
Gabriela Hermenegildo Júnior <gabriela.hermenegil@meta.com.br>
2022-12-30 16:45:54 -0300

    creating menu

--- exercise-2/src/app/components/header/header.module.ts
+++ exercise-2/src/app/components/header/header.module.ts
@@ -1,3 +1,4 @@
+import { MenuModule } from './../menu/menu.module';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -10,7 +11,7 @@ import { HeaderComponent } from './header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule, RouterModule
+    CommonModule, RouterModule, MenuModule
   ],
   exports: [HeaderComponent]
 })
